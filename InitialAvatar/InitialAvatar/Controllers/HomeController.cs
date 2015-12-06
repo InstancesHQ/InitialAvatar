c1d7852ec4b106c0b945459606a9701a088491a6
Patrick Dunn <pat.dunn@outlook.com>
2015-12-06 18:54:40 -0500

    move draw logic into a manager. clean up pixels around text.

--- InitialAvatar/InitialAvatar/Controllers/HomeController.cs
+++ InitialAvatar/InitialAvatar/Controllers/HomeController.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using InitialAvatar.Managers;
+using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
@@ -21,25 +23,8 @@ namespace InitialAvatar.Controllers
         [Route("avatar/{initials}")]
         public FileResult DownloadImage(string initials)
         {
-            var fontFamily = new FontFamily("Arial");
-            var font = new Font(fontFamily, 60, FontStyle.Regular, GraphicsUnit.Pixel);
-            var stringFormat = new StringFormat();
-            stringFormat.LineAlignment = StringAlignment.Center;
-            stringFormat.Alignment = StringAlignment.Center;
-            var img = new Bitmap(120, 120);
-            var drawing = Graphics.FromImage(img);
-            drawing.Clear(Color.Blue);
-            var textBrush = new SolidBrush(Color.White);
-            var pointf = new Point(60, 66);
-            drawing.DrawString(initials, font, textBrush, pointf, stringFormat);
-            drawing.Save();
-            textBrush.Dispose();
-            drawing.Dispose();
-            using (var memory = new MemoryStream())
-            {
-                img.Save(memory, ImageFormat.Jpeg);
-                return File(memory.ToArray(), "image/jpeg");
-            }
+            var manager = new DrawingManager();
+            return File(manager.DrawInitialAvatar(initials), "image/png");
         }
     }
 }
---
+++ InitialAvatar/InitialAvatar/Managers/DrawingManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace InitialAvatar.Managers
+{
+    public class DrawingManager
+    {
+        public byte[] DrawInitialAvatar(string initials)
+        {
+            initials = initials.ToUpper();
+            var fontSize = initials.Length > 2 ? 30 : 40;
+            var font = new Font("Arial", fontSize);
+            var stringFormat = new StringFormat
+            {
+                LineAlignment = StringAlignment.Center,
+                Alignment = StringAlignment.Center,
+                FormatFlags = StringFormatFlags.FitBlackBox
+            };
+            var image = new Bitmap(120, 120);
+            var drawing = Graphics.FromImage(image);
+            var rect = new Rectangle(0, 4, 120, 120);
+            drawing.Clear(Color.Blue);
+            drawing.DrawString(initials, font, Brushes.White, rect, stringFormat);
+            drawing.Flush();
+            drawing.Dispose();
+            using (var memory = new MemoryStream())
+            {
+                image.Save(memory, ImageFormat.Png);
+                return memory.ToArray();
+            }
+        }
+    }
+}
